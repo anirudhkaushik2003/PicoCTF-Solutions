eab31480e652810a15c3c9a429f3fd164794a67c
anirudhkaushik2003 <anirudh.kaushik@students.iiit.ac.in>
2021-05-02 01:07:40 +0530

    Done

---
+++ picoCTF/remote.py
@@ -0,0 +1,10 @@
+import numpy as np
+array = []
+for i in range(42):
+    temp  = int(input())
+    array.append(temp)
+for i in range(42):
+    ascii = chr(array[i])
+    print(ascii,end = "")
+print('\n')
+   
